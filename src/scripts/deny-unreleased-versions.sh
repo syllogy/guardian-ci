cce8a0b98c45f04809af03174794885f4cd5e27b
Tim Deeb-Swihart <chronoslynx@users.noreply.github.com>
2020-12-18 12:13:15 -0500

    || true it is...

--- src/scripts/deny-unreleased-versions.sh
+++ src/scripts/deny-unreleased-versions.sh
@@ -1,7 +1,5 @@
-set +o pipefail
-matches=$(go mod graph | awk '{print $2}' | grep 'github.com/BishopFox' | grep -E -v 'v[0-9]+.[0-9]+.[0-9]+$' | grep -v groot); \
+matches=$(go mod graph | awk '{print $2}' | grep 'github.com/BishopFox' | grep -E -v 'v[0-9]+.[0-9]+.[0-9]+$' | grep -v groot || true); \
 test -z "$matches" || (printf "\nfailing build as go.mod references unreleased library versions: \n%s\n" "$matches" >&2 && exit 1);
 
-matches=$(grep -e '^replace' go.mod); \
+matches=$(grep -e '^replace' go.mod || true); \
 test -z "$matches" || (printf "\nfailing build as go.mod contains replace statements: \n%s\n" "$matches" >&2 && exit 1);
-set -o pipefail
