537ecb7d2799cc72a544b080562ef8f49cffb809
Max Eshleman <58948157+maxeshleman@users.noreply.github.com>
2021-02-09 10:40:08 -0700

    [semver:patch] improve preflight messaging (#23)
    * improve preflight messaging

---
+++ src/scripts/preflight.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+pr_number=$(echo "$CIRCLE_PULL_REQUEST" | cut -d / -f 7)
+draft_pr_status_url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$pr_number?access_token=$GITHUB_TOKEN"
+pr_status_result=$(curl --silent -H "Accept: application/vnd.github.shadow-cat-preview+json" "$draft_pr_status_url")
+not_draft_pr=$(echo "$pr_status_result" | jq '.draft == false')
+
+
+if ${not_draft_pr}; then
+  echo "Not a draft PR, running pipeline..."
+else
+  echo "PR status: $pr_status_result"
+  echo "This is a draft pr. Skipping..."
+  exit 1
+fi
