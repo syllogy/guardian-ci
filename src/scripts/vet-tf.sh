b2a1da91bd7c7af61d3efa2d250175f3c375930f
Max Eshleman <58948157+maxeshleman@users.noreply.github.com>
2021-02-26 12:53:46 -0700

    [semver:patch] fix vet tf (#27)
    * fix vet tf

--- src/scripts/vet-tf.sh
+++ src/scripts/vet-tf.sh
@@ -1,13 +1,16 @@
-
 terraform fmt --check . || (echo "Terraform is not formatted correctly. Please run 'terraform fmt'" && exit 1)
 
 latest_module_version="v$(cat ~/terraform-modules/VERSION)"
 
-grep -E 'source = .*bishopfox.*v[0-9\.]*' ./*.tf | grep -oE 'v[0-9\.]+' || true | while read -r version; do
-  if [[ "$latest_module_version" != "$version" ]]; then
-    echo "terraform modules can be upgraded from $version to $latest_module_version"
-    echo "if it cannot be upgraded, remove this job from the pipeline and re-add it when modules can be upgraded"
+tf_versions=$(grep -E 'source = .*bishopfox.*v[0-9\.]*' ./*.tf | grep -oE 'v[0-9\.]+')
 
-    exit 1
-  fi
-done
+if [[ -n $tf_versions ]]; then
+  echo "$tf_versions" | while read -r version; do
+    if [[ "$latest_module_version" != "$version" ]]; then
+      echo "terraform modules can be upgraded from $version to $latest_module_version"
+      echo "if it cannot be upgraded, remove this job from the pipeline and re-add it when modules can be upgraded"
+
+      exit 1
+    fi
+  done
+fi
