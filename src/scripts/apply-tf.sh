9798bf3060f4bc649fe6705e3e8b4841deaa047c
Max Eshleman <58948157+maxeshleman@users.noreply.github.com>
2021-04-20 10:45:51 -0600

    [semver:patch] don't require default tf vars to be used (#38)

--- src/scripts/apply-tf.sh
+++ src/scripts/apply-tf.sh
@@ -21,5 +21,9 @@ if [ -n "${USE_VERSION_SHA:-}" ]; then
   VERSION="$VERSION-${CIRCLE_SHA1:-}"
 fi
 
+export TF_VAR_app_version="$VERSION" \
+  TF_VAR_env="$ENV_NAME" \
+  TF_VAR_aws_profile="$AWS_PROFILE"
+
 aws ssm get-parameter --name "/cast/${ENV_NAME}/vars/${SERVICE}" --query 'Parameter.Value' --output text > vars.auto.tfvars
-terraform apply --var "app_version=$VERSION" --var "env=$ENV_NAME" --var "aws_profile=$AWS_PROFILE" --auto-approve
+terraform apply --var "aws_profile=$AWS_PROFILE" --auto-approve
